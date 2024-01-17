8b3079b94a9835abe0754343a9bcfa25da43f0db
Pi-Tato <carloah314@gmail.com>
2024-01-17 13:35:56 -0700

    Added an About Me

---
+++ scripts/w02-task.js
@@ -0,0 +1,39 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+
+let fullName = "Noah Carlisle";
+let currentYear = new Date();
+let profilePicture = `images/profile.jpg`;
+
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById("profilePicture")
+
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear.getFullYear();
+imageElement.setAttribute(`src`, profilePicture);
+imageElement.setAttribute(`alt`, `Profile image of ${fullName}`);
+
+
+/* Step 5 - Array */
+
+let favFoods = ['Steak', ' Spaghetti', ' Pizza',' Pumkin Pie', ' Banana Cream Pie',
+' Egg Nog Pie', ' Lasagna'];
+
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.push(' Pancakes');
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
